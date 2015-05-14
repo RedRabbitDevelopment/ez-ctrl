0667d6772c84d53abc9213ab8f589769717cfc44
Nathan Tate <nathantate88@gmail.com>
2015-05-14 11:20:16 -0600

    Moved to src directory

--- new-use-cases.js
+++
@@ -1,9 +0,0 @@
-- Use es6, not coffeescript
-- Remove success: true, response
-- Include proper status codes
-- Run server-side and client-side
-- Render other formats, csv, html, etc
-- Socket.io
-- Client side validation
-- put validation, socket/express, ez-access, and gulp ez on different modules
-
